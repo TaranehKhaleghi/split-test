27d79cb515fc3c3f0862470fdefe87bcde94048e
TaranehKhaleghi <tkhaleghi5062@conestogac.on.ca>
2020-11-20 16:27:17 -0500

    first initial

--- test1/index.js
+++ test1/index.js
@@ -1,55 +1,205 @@
+Survey.StylesManager.applyTheme("modern");
 
-Survey
-    .StylesManager
-    .applyTheme("default");
-
-var json = {
-    title: "American History",
-    pages: [
-        {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "civilwar",
-                    title: "When was the Civil War?",
-                    choices: [
-                        "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
-                    ],
-                    correctAnswer: "1850-1900"
-                }
-            ]
-        }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "libertyordeath",
-                    title: "Who said 'Give me liberty or give me death?'",
-                    choicesOrder: "random",
-                    choices: [
-                        "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
-                    ],
-                    correctAnswer: "Patrick Henry"
-                }
-            ]
-        }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "magnacarta",
-                    title: "What is the Magna Carta?",
-                    choicesOrder: "random",
-                    choices: [
-                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
-                    ],
-                    correctAnswer: "The foundation of the British parliamentary system"
-                }
-            ]
-        }
+var surveyJSON = {
+    "title": "Employment screening",
+    "focusOnFirstError": false,
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "radiogroup",
+        "name": "question1",
+        "title": "What is your relationship with the candidate?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Manager"
+         },
+         {
+          "value": "item2",
+          "text": "Director"
+         },
+         {
+          "value": "item3",
+          "text": "Coworker"
+         }
+        ]
+       },
+       {
+        "type": "radiogroup",
+        "name": "question2",
+        "title": "Have you ever worked with the candidate on a professional capacity?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
+        ]
+       },
+       {
+        "type": "checkbox",
+        "name": "question4",
+        "title": "In your experience with the candidate, how does he/she handle stress? Conflict? Pressure?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": " Try to react to situations rather than to stress"
+         },
+         {
+          "value": "item2",
+          "text": "Talk with the other person"
+         },
+         {
+          "value": "item3",
+          "text": "Prioritize the areas of conflict"
+         }
+        ],
+        "hasOther": true
+       },
+       {
+        "type": "radiogroup",
+        "name": "question9",
+        "title": "If you were the hiring manager, would you hire the candidate?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
+        ]
+       },
+       {
+        "type": "radiogroup",
+        "name": "question5",
+        "title": "Would you trust the candidate with large sums of money, children, or sensitive information? ",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Yes"
+         },
+         {
+          "value": "item2",
+          "text": "No"
+         }
+        ]
+       },
+       {
+        "type": "checkbox",
+        "name": "question7",
+        "title": "Were there any behaviors that impacted his/her job performance?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Being late"
+         },
+         {
+          "value": "item2",
+          "text": "Missing deadlines"
+         },
+         {
+          "value": "item3",
+          "text": "arguing with colleagues"
+         }
+        ],
+        "hasOther": true
+       },
+       {
+        "type": "radiogroup",
+        "name": "question10",
+        "title": "How would you rate the applicant’s job performance?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "1-3"
+         },
+         {
+          "value": "item2",
+          "text": "4-6"
+         },
+         {
+          "value": "item3",
+          "text": "7-10"
+         }
+        ]
+       },
+       {
+        "type": "checkbox",
+        "name": "question8",
+        "title": "Why did he/she leave your company?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "looking for better career prospects"
+         },
+         {
+          "value": "item2",
+          "text": "Office politics"
+         },
+         {
+          "value": "item3",
+          "text": "More salary"
+         }
+        ],
+        "hasOther": true,
+        "hasSelectAll": true
+       },
+       {
+        "type": "checkbox",
+        "name": "question3",
+        "title": "What are some of his/her traits that you believe will make them a strong hire?",
+        "choices": [
+         {
+          "value": "item1",
+          "text": "Communication"
+         },
+         {
+          "value": "item2",
+          "text": "Teamwork"
+         },
+         {
+          "value": "item3",
+          "text": "Analytical Skills"
+         },
+         {
+          "value": "item4",
+          "text": "Strong Work Ethic"
+         }
+        ]
+       },
+       {
+        "type": "text",
+        "name": "question6",
+        "title": "How does the candidate interact with others?"
+       }
+      ]
+     },
+     {
+      "name": "page2"
+     }
     ],
-    completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
-};
+    "storeOthersAsComment": false
+   }
 
-window.survey = new Survey.Model(json);
+/*function sendDataToServer(survey) {
+    //send Ajax request to your web server.
+     JSON.stringify(survey.data);
+}
+
+//var survey = new Survey.Model(surveyJSON);
+/*$("#surveyContainer").Survey({
+    model: survey,
+    onComplete: sendDataToServer
+});*/
+
+window.survey = new Survey.Model(surveyJSON);
 
 survey
     .onComplete
